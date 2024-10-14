e8ed04c575ac0fce58fbb8956f1fed00b790b25f
Emily Zhou <zhou@middlebury.edu>
2024-10-14 00:21:34 -0400

    main js file

--- js/main.js
+++ js/main.js
@@ -16,8 +16,7 @@ const lollipopEL = d3.select('#lollipop');
 lollipop(lollipopEL, severityData);
 
 // Draw the donut charts
-const palette1 = ['#9D1F35', '#16335D', '#B2BEB5', '#CC2929', '#6E726E','#8592A0']; // dark red, de blue, green, charcol, 
-const palette = ['#8592A0', '#16335D', '#B2BEB5', '#6E726E','#CC2929', '#9D1F35', '#CE5555', '#878787']; // dark red, de blue, green, charcol,
+const palette = ['#8592A0', '#16335D', '#B2BEB5', '#6E726E', '#CC2929', '#9D1F35', '#CE5555', '#878787']; // dark red, de blue, green, charcol,
 const donut1EL = d3.select('#donut1');
 const input = 'Type';
 drawDonut(donut1EL, input, typeData, palette);
