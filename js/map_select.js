fe4a43d3c6e2614feafdf65e03083b14959b544c
xllee07 <xianlulee@gmail.com>
2024-10-24 14:41:57 -0400

    update marker sizes and text

--- js/map_select.js
+++ js/map_select.js
@@ -171,8 +171,8 @@ populatesentiment();
         data: geojson
       },
       paint: {
-        'circle-radius': 2,
-        'circle-opacity': 0.7,
+        'circle-radius': 3,
+        'circle-opacity': 0.5,
         'circle-color': ['match', ['get', 'sentiment'], // get the property
         'Happy', '#56949A',
         'Unhappy', '#D7677B',
